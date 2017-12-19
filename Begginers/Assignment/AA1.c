18aaf8163d9d3af9874cd5352470525a6944e348
Priya2h12 <34653614+Priya2h12@users.noreply.github.com>
2017-12-19 11:00:35 +0530

    Update AA1.c

--- Begginers/Assignment/AA1.c
+++ Begginers/Assignment/AA1.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+int main(void) 
 {
 int number;
 printf("Enter a number\n");
 scanf("%d",&number);
 if(number>=0)
-printf("%d is a positive number\n",number);
+printf(" Entered number is a positive number\n",number);
 else
-printf("%d is a negative number\n",number);
+printf(" Entered number is a negative number\n",number);
+	return 0;
 }
