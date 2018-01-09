3a9d3048858902c23cf1123d3fff03222355e157
Priya2h12 <34653614+Priya2h12@users.noreply.github.com>
2018-01-09 15:12:55 +0530

    Update prime number or not.c

--- Begginers/Assignment/prime number or not.c
+++ Begginers/Assignment/prime number or not.c
@@ -1,1 +1,43 @@
 
+#include<stdio.h>
+int main()
+{
+int n,i,flag = 0;
+scanf("%d",&n); 
+
+int div = n/2;
+for(i=2;i<div;i++)
+
+{
+    if(n%i==0)
+    {
+        flag = 1;
+        break;
+    }
+}
+
+if(flag == 0)
+{
+    int temp,rev=0,rem;
+  
+   temp = n;
+   while (n!=0)
+   {
+       rem = n%10;
+       rev = (rev*10)+rem;
+       n =n/10;
+   }
+
+ if(temp == rev)
+{
+    printf("yes");
+    
+}
+}
+
+else
+{
+    printf("not");
+   return 0; 
+}
+}
