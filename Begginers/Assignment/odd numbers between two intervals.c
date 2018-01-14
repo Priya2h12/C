b801dadfff327345ccb0fb6ef2f1a64689314dc9
Priya2h12 <34653614+Priya2h12@users.noreply.github.com>
2018-01-14 06:56:17 -0800

    Add files via upload

---
+++ Begginers/Assignment/odd numbers between two intervals.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void)
+{
+    int n1,n2,i;
+    scanf("%d %d",&n1,&n2);
+    for(i=n1+1;i<n2;i++)
+    {
+         if((i%2)!==0)
+         {
+         printf(" %d",i);
+         }
+    }
+    return 0;
+}
