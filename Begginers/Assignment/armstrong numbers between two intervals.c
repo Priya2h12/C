8ab377c3b2e7ae2ca63b52dc46a60ccb0d1ddd35
Priya2h12 <34653614+Priya2h12@users.noreply.github.com>
2018-01-14 06:03:04 -0800

    Add files via upload

---
+++ Begginers/Assignment/armstrong numbers between two intervals.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+#include <math.h>
+
+int main(void)
+
+{
+    
+int n1,n2, i, t1, t2, r,n = 0,res=0;
+   
+printf("Enter two numbers: ");
+    
+scanf("%d %d", &n1,&n2);
+    
+printf("Armstrong numbers between %d an %d are: ",n1,n2);
+    
+for(i=n1+1;i<n2;++i)
+    
+{
+        
+t2=i;
+        
+t1=i;
+        
+while(t1!=0)
+        
+{
+            
+t1 /=10;
+            
+++n;
+        
+}
+        
+while(t2!=0)
+        
+{
+            
+r= t2%10;
+            
+res +=pow(remainder, n);
+            
+t2 /= 10;
+        
+}
+
+if(res==i) 
+        
+{
+            
+printf("%d ", i);
+        
+}
+        
+n=0;
+        
+res=0;
+
+    
+}
+    
+return 0;
+
+}
