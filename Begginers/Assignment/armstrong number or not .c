33c46faa7567fb2f4672f57f57317542d7b05418
Priya2h12 <34653614+Priya2h12@users.noreply.github.com>
2018-01-14 05:33:22 -0800

    Add files via upload

---
+++ Begginers/Assignment/armstrong number or not .c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+
+int main(void) 
+{
+   
+int n,temp,s=0,r;
+    
+printf("\nEnter number for checking Armstrong : ");
+   
+scanf("%d",&n);
+ 
+temp=n;
+ 
+while(n!=0) 
+ 
+{
+      
+r=n%10;
+      
+s=s+(r * r * r);
+      
+n=n/10;
+   
+}
+
+if(temp==s)
+      
+printf("yes");
+   
+else
+      
+printf("no");
+
+   
+return (0);
+
+}
