a5104d8604feb038663d5a0a43f43124eee07fb5
Priya2h12 <34653614+Priya2h12@users.noreply.github.com>
2018-01-14 06:13:02 -0800

    Add files via upload

---
+++ Begginers/Assignment/factorial of a number.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    
+int n, i;
+    
+unsigned long long factorial = 1;
+
+    
+printf("Enter an integer: ");
+    
+scanf("%d",&n);
+     
+if(n<0)
+     
+printf("Factorial of a negative number doesn't exist.");
+     
+else
+    
+{
+    
+for(i=1;i<=n;++i)
+        
+{
+            
+factorial *= i;             
+        
+}
+        
+printf("Factorial of %d = %llu", n, factorial);
+    
+}
+
+    
+return 0;
+
+}
+
