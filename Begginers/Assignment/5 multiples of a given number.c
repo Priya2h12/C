540c35b18f35574f92c0a43311f3a0b930703e62
Priya2h12 <34653614+Priya2h12@users.noreply.github.com>
2018-01-14 06:48:00 -0800

    Add files via upload

---
+++ Begginers/Assignment/5 multiples of a given number.c
@@ -0,0 +1,28 @@
+#include<stdio.h> 
+
+void main() 
+
+{ 
+ 
+int r, c, y, n ; 
+ 
+printf("Enter a number: ");
+ 
+scanf("%d", &n);
+ 
+if(c = 1); 
+  
+do 
+  
+{ 
+   
+y=n*c; 
+   
+printf(" %d ", y) ; 
+   
+c = c + 1 ; 
+  
+} 
+while(c <=5) ; 
+
+} 
