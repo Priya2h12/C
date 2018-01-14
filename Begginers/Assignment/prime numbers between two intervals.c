f60555f7f4449d391dd3339971a2480eb8d9f460
Priya2h12 <34653614+Priya2h12@users.noreply.github.com>
2018-01-14 05:17:49 -0800

    Add files via upload

---
+++ Begginers/Assignment/prime numbers between two intervals.c
@@ -0,0 +1,49 @@
+include <stdio.h>
+
+int main()
+
+{
+    
+int n1,n2,i,flag;
+    
+printf("Enter two numbers: ");
+    
+scanf("%d %d", &n1,&n2);
+    
+printf("Prime numbers between %d and %d are: ",n1,n2);
+    
+while(n1<n2)
+    
+{
+        
+flag = 0;
+        
+for(i=2;i<=n1/2;++i)
+        
+{
+            
+if(n1%i==0)
+            
+{
+
+                
+flag=1;
+                
+break;
+            
+}
+        
+}
+        
+if(flag==0)
+        
+printf(" %d ",n1);
+        
+++n1;
+    
+}
+
+    
+return 0;
+
+}
