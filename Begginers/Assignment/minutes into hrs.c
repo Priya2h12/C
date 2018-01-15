b4bdc2f57c27b4a35e298080dcb1909f7cb938b9
Priya2h12 <34653614+Priya2h12@users.noreply.github.com>
2018-01-15 06:30:12 -0800

    Add files via upload

---
+++ Begginers/Assignment/minutes into hrs.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+
+{
+    
+int minutes,hours;
+    
+printf("enter time in minutes:");
+    
+scanf("%d",&minutes);
+    
+{
+        
+hours=minutes/60;
+    
+}
+    
+printf("%dhours",hours);
+   
+
+}
