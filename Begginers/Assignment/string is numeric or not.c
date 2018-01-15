bd2b8ececf2161311df7c399b9a59d919978b5b1
Priya2h12 <34653614+Priya2h12@users.noreply.github.com>
2018-01-15 06:08:26 -0800

    Add files via upload

---
+++ Begginers/Assignment/string is numeric or not.c
@@ -0,0 +1,37 @@
+#include<stdio.h>    
+void main()
+    
+{
+    
+char s[30];
+    
+int i,flag=0,n=0;
+    
+printf("enter the string:");
+    
+scanf("%s",s);
+    
+for(i=0;s[i]!='\0';i++)
+    
+{
+    
+if(s[i]=='0'||s[i]=='1'||s[i]=='2'||s[i]=='3'||s[i]=='5'||s[i]=='6'||s[i]=='7'||s[i]=='8'||s[i]=='9')
+    
+{
+    
+flag=flag+1;
+    
+}
+    
+}
+    
+if(flag==0)
+    
+printf("given string  is not numeric");
+    
+else
+    
+printf("given string is numeric");
+    
+}
+
