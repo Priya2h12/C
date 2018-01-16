542a3de9d4150cfbc3b2c699e0f9faa3c09f8a34
Priya2h12 <34653614+Priya2h12@users.noreply.github.com>
2018-01-16 01:11:05 -0800

    Add files via upload

---
+++ Begginers/Assignment/no.of spaces in a line.c
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		int len=str.split(" ").length;
+	            System.out.println(len-1);
+	}
+}
