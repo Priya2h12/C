71cb01e5c48b8a974a16744f29d783aea641365c
Priya2h12 <34653614+Priya2h12@users.noreply.github.com>
2018-01-16 08:28:25 -0800

    Add files via upload

---
+++ Begginers/Assignment/number between 1 to 10.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(void) 
+
+{
+
+int n;
+
+printf("enter a number:");
+
+scanf("%d",&n);
+
+{
+    
+if(n<='1'&&n<='9')
+
+{
+
+printf("yes");
+
+}
+
+else
+
+printf("no");
+
+}
+
+}
