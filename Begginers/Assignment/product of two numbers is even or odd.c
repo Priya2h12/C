af76e5a56ed590e4b23b4c2807422b6c17d9321b
Priya2h12 <34653614+Priya2h12@users.noreply.github.com>
2018-01-21 06:43:45 -0800

    Add files via upload

---
+++ Begginers/Assignment/product of two numbers is even or odd.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+
+{
+    int n,m,s;
+    
+printf("enter the number");
+    
+scanf("%d %d",&n,&m);
+    
+s=n*m;
+    
+{
+    
+if(s%2==0)
+
+printf("even");
+
+else
+
+printf("odd");
+
+}
+
+}
