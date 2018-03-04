5fb7bceabb4577a11d0e5a9fa8b0973446693ff8
Priya2h12 <34653614+Priya2h12@users.noreply.github.com>
2018-03-04 06:31:17 -0800

    Add files via upload

---
+++ Begginers/Assignment/addition of two numbers is odd or even.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+int main() 
+{
+   
+int x=9;
+   
+int y=2;
+   
+int z=x+y;
+   
+if(z%2==0)
+  
+ {
+       
+printf("even");
+  
+ }
+  
+ else
+  
+ printf("odd");
+  
+ }
