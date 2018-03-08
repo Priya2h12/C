c2ccea22c883d324ea30df219a993db9be01dafd
Priya2h12 <34653614+Priya2h12@users.noreply.github.com>
2018-03-08 07:14:36 -0800

    Add files via upload

---
+++ Begginers/Assignment/multiples of 13.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+int main(void) 
+
+{
+
+	int a;
+
+	printf("enter the number:");
+
+	scanf("%d",&a);
+
+	if(a%13==0)
+
+	{
+
+		printf("\n yes");
+
+	}
+
+	else
+
+	{
+
+		printf("\n no");
+
+	}
+
+	return 0;
+
+}
