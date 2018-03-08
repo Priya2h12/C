312e7111d457e44558d5334b049a2bd465c36095
Priya2h12 <34653614+Priya2h12@users.noreply.github.com>
2018-03-08 06:48:29 -0800

    Add files via upload

---
+++ Begginers/Assignment/round of number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+int main(void) 
+{
+	float n;
+	int n1;
+	printf("\n enter the num:");
+	scanf("%f",&n);
+	n1=(int)(n+0.5);
+	printf("\n%d",n1);
+	return 0;
+}
