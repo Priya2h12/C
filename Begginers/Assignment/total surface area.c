e1f8e53193e3f9f34b1270299d8e92aa8c6152a2
Priya2h12 <34653614+Priya2h12@users.noreply.github.com>
2018-03-08 09:06:17 -0800

    Add files via upload

---
+++ Begginers/Assignment/total surface area.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int len,bred,heig;
+	scanf("%d %d %d",&len,&brea,&heig);
+	int volume,tsa;
+	volume=len*bred*heig;
+	tsa=2*((len*brea)+(brea*heig)+(heig*l));
+	printf("\nVOLUME=%d",volume);
+	printf("\nTSA=%d",tsa);
+	return 0;
+}
